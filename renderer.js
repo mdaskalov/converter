c7b310fb76cb04df6f6c2952704acb56cbf65814
Milko Daskalov <milko.daskalov@mpay24.com>
2019-12-30 12:29:25 +0100

    file and dialog logic moved in the renderer

--- renderer.js
+++ renderer.js
@@ -4,7 +4,7 @@
 
 const { dialog } = require('electron').remote
 
-const fs = require('fs')
+const fs = require('fs').promises
 const path = require('path')
 const InputView = require('./src/input')
 const OutputVeiw = require('./src/output')
@@ -14,40 +14,34 @@ const outputView = new OutputVeiw()
 
 const views = [inputView, outputView];
 
-selectInputFile = done => {
-  dialog.showOpenDialog({
-      filters: [{
-        name: 'Base64 Files',
-        extensions: ['b64']
-      }],
-      properties: ['openFile']
-
-    })
-    .then(result => {
-      var relativePath
-      if (!result.canceled && result.filePaths.length >= 1) {
-        relativePath = path.relative(process.cwd(), result.filePaths[0]);
-      }
-      done(relativePath)
-    })
+selectInputFile = async () => {
+  result = await dialog.showOpenDialog({
+    filters: [{
+      name: 'Base64 Files',
+      extensions: ['b64']
+    }],
+    properties: ['openFile']
+  })
+  var relativePath
+  if (!result.canceled && result.filePaths.length >= 1) {
+    relativePath = path.relative(process.cwd(), result.filePaths[0]);
+  }
+  return relativePath
 }
 
-selectOutputFile = done => {
-  dialog.showSaveDialog({
+selectOutputFile = async () => {
+  result = await dialog.showSaveDialog({
       filters: [{
         name: 'XML Files',
         extensions: ['xml']
       }],
       defaultPath: 'output.xml'
-
-    })
-    .then(result => {
-      var relativePath
-      if (!result.canceled && result.filePath) {
-        relativePath = path.relative(process.cwd(), result.filePath);
-      }
-      done(relativePath)
-    })
+  })
+  var relativePath
+  if (!result.canceled && result.filePath) {
+    relativePath = path.relative(process.cwd(), result.filePath);
+  }
+  return relativePath
 }
 
 const deactivateDocLinks = () => {
@@ -66,6 +60,9 @@ const renderDataPane = viewName => {
   const view = views.find(v => v.name == viewName)
   if (view) {
     document.querySelector('#data-pane').innerHTML = view.render();
+    deactivateDocLinks()
+    activateDocLink(viewName)
+
     // bind input textarea events
     let inputData = document.querySelector('#input-data')
     if (inputData) {
@@ -83,81 +80,68 @@ Array.prototype.forEach.call(docButtons, button => {
   const viewName = button.id.substring(0, button.id.indexOf("-view"))
   if (viewName != undefined) {
     button.addEventListener('click', () => {
-      deactivateDocLinks()
       renderDataPane(viewName)
-      activateDocLink(viewName)
     })
   }
 })
 
-document.querySelector(`#select-input-file`).addEventListener('click', () => {
+document.querySelector(`#select-input-file`).addEventListener('click', async () => {
   document.querySelector(`#select-input-file`).classList.add('active')
-  selectInputFile(fileName => {
-    document.querySelector('#select-input-file').classList.remove('active')
-    if (fileName) {
-      inputView.parseFile(fileName)
-      .then(input => {
-        deactivateDocLinks()
-        activateDocLink('output')
-        inputView.data = input
-        inputView.decompress(input)
-        .then(output => {
-          outputView.setData(output)
-          renderDataPane('output')
-        })
-        .catch(err => {
-          outputView.setError(err)
-          renderDataPane('output')
-        })
-      })
-      .catch(err => {
-        inputView.setError(err)
-        deactivateDocLinks()
-        activateDocLink('input')
-        renderDataPane('input')
-      })
+  let fileName = await selectInputFile()
+  if (fileName) {
+    deactivateDocLinks()
+    try {
+      let data = await fs.readFile(fileName)
+      inputView.setData(data)
+      let converted = await inputView.convert(data.toString())
+      outputView.setData(converted)
     }
-  })
+    catch(err) {
+      outputView.setError(err)
+    }
+    renderDataPane('output')
+  }
+  document.querySelector(`#select-input-file`).classList.remove('active')
 })
 
-document.querySelector('#decompress-button').addEventListener('click', () => {
-  deactivateDocLinks()
+document.querySelector('#convert-button').addEventListener('click', async () => {
+  document.querySelector(`#convert-button`).classList.add('active')
   if (inputView.data) {
-    activateDocLink('output')
-    inputView.decompress(inputView.data)
-    .then(result => {
-      outputView.setData(result)
-      outputView.error = undefined
-      renderDataPane('output')
-      })
-    .catch(err => {
-      outputView.error = err
-      renderDataPane('output')
-    })
+    deactivateDocLinks()
+    try {
+      let converted = await inputView.convert(inputView.data)
+      outputView.setData(converted)
+    }
+    catch(err) {
+      outputView.setError(err)
+    }
+    renderDataPane('output')
   }
   else {
     dialog.showErrorBox('Nothing to convert', 'Load the input data first.')
   }
+  document.querySelector(`#convert-button`).classList.remove('active')
 })
 
-document.querySelector(`#select-output-file`).addEventListener('click', () => {
+document.querySelector(`#select-output-file`).addEventListener('click', async () => {
   document.querySelector(`#select-output-file`).classList.add('active')
   if (outputView.data) {
-    selectOutputFile(fileName => {
-      document.querySelector('#select-output-file').classList.remove('active')
-      if (fileName) {
-        output.saveFile(fileName, output.xml, () => {
-          deactivateDocLinks()
-          activateDocLink('output')
-          renderDataPane('output')
-        })
+    let fileName = await selectOutputFile();
+    if (fileName) {
+      deactivateDocLinks()
+      try {
+        await fs.writeFile(fileName, outputView.data)
       }
-    })
+      catch(err) {
+        dialog.showErrorBox('Error writting file', err.toString())
+      }
+      renderDataPane('output')
+    }
   }
   else {
-    document.querySelector('#select-output-file').classList.remove('active')
     dialog.showErrorBox('Nothing to convert', 'Load the input data first.')
   }
+  document.querySelector('#select-output-file').classList.remove('active')
 })
 
 // Global
